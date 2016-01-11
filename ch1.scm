85860f92b43b38b82c72202e20cbe2880d6b5d11
Scott McLeod <halcyonblue@gmail.com>
2016-01-10 23:18:56 -0500

    Add Newton's method

--- ch1.scm
+++ ch1.scm
@@ -37,3 +37,21 @@
 
 (define (sum-of-squares-first-two-largest x y z)
   (apply sum-of-squares (first-two-largest x y z)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
